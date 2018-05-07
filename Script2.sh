1f589a96ceb393f12c1dfd0cd8c53f8711487c50
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-05-07 10:22:51 -0700

    Create Script2.sh

---
+++ Script2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#create a file 
+touch File97
+
+#create a new directory
+mkdir Sale1
+
+#lists the contents of directory 
+ls 
+
+#move File97 to Sale1
+mv File97 /home/user/Sale1
+
+#create another file
+touch File98
+
+#create another new directory 
+mkdir Sale2
+
+#move File98 to Sale2
+mv File98  /home/user/Sale2
+
+#create another new directory 
+mkdir Sale3
+
+#lists all the contents of directory 
+ls –all
+
+#end
+clear
